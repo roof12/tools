fa1067c1edbae41c657eca6b990fade5be78eaa0
Scott Lewis <scott@roof12.com>
2023-10-16 07:11:21 -0600

    add-remote-all.sh

---
+++ utilities/add-remote-all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+repos='gitea codeberg github bogus'
+
+found=0
+
+for repo in $repos; do
+
+	url=""
+	url=$(git remote get-url $repo)
+
+	if [ $url ]; then
+
+		if [ $found = 0 ]; then
+			found=1
+			git remote add all "$url"
+		fi
+
+		git remote set-url --add --push all $url >/dev/null 2>&1
+		echo Adding $repo to remote all
+	fi
+
+done
